3e543c3afecf71c4c54855aab20cec41ae5a6e92
Evgeny_Ch <gek_sh@mail.ru>
2024-04-11 01:24:01 +0300

    Added check param name

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -3,11 +3,19 @@ package org.example.hexlet;
 import io.javalin.Javalin;
 
 public class HelloWorld {
-    public static void main(String[] args) {
-        // Создаем приложение
-        var app = Javalin.create(config -> config.bundledPlugins.enableDevLogging());
-        // Описываем, что загрузится по адресу
-        app.get("/", ctx -> ctx.result("Hello World"));
-        app.start(7070);
-    }
+     public static void main(String[] args) {
+          // создаем приложение
+          var app = Javalin.create(config -> config.bundledPlugins.enableDevLogging());
+          // описываем 
+          app.get("/hello", ctx -> {
+              var name = ctx.queryParam("name");
+              //if (ctx.queryParam("name").isEmpty()) {
+              if ((name == null) || (name.isEmpty())) {
+                    name = "World";
+              }
+              ctx.result("Hello, " + name);
+          });
+          app.start(7070);
+     }
 }
+
