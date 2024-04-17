e5469dce7aed7e1a08e68c3050a2c69104662d8b
Evgeny_Ch <gek_sh@mail.ru>
2024-04-18 01:52:08 +0300

    Added model entity

---
+++ src/main/java/org/example/hexlet/model/Course.java
@@ -0,0 +1,34 @@
+package org.example.hexlet.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Course {
+
+    private long id;
+
+    @ToString.Include
+    private String name;
+    private String description;
+
+    public Course(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public long getId() {
+        return id;
+    }
+}
