857eac5b93be5889ca6dd7af01f7e8a6e5073630
Evgeny_Ch <gek_sh@mail.ru>
2024-04-26 22:55:54 +0300

    Added route /courses and /

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,9 +1,11 @@
 package org.example.hexlet;
 
 import io.javalin.Javalin;
+import io.javalin.http.NotFoundResponse;
 import io.javalin.rendering.template.JavalinJte;
 import static io.javalin.rendering.template.TemplateUtil.model;
 
+import org.example.hexlet.dto.courses.CoursePage;
 import org.example.hexlet.dto.courses.CoursesPage;
 import org.example.hexlet.model.Course;
 
@@ -20,18 +22,40 @@ public class HelloWorld {
           // Обратите внимание, что id — это не обязательно число
 
           app.get("/courses/{id}", ctx -> {
-               var id = ctx.pathParam("id");
+               var id = ctx.pathParamAsClass("id", Long.class).get();
+               var courses = List.of(new Course("Java", "Программирование на Java", 1),
+                       new Course("JavaScript", "Программирование на JavaScript", 2),
+                       new Course("Go", "Программирование на Go", 3));
                //var course = /* Курс извлекается из базы данных */
-                       // Предполагаем, что у курса есть метод getName()
-               var courses = List.of(new Course("Java", "Программирование на Java"),
-                       new Course("JavaScript", "Программирование на JavaScript"),
-                       new Course("Go", "Программирование на Go"));
+               var course = findCourse(courses, id);
+               if (course == null) {
+                    throw new NotFoundResponse("Course not found");
+               }
+               var page = new CoursePage(course);
+               ctx.render("courses/show.jte", model("page", page));
+          });
+
+          app.get("/courses", ctx -> {
+               //var courses = /* Список курсов извлекается из базы данных */
+               var courses = List.of(new Course("Java", "Программирование на Java", 1),
+                       new Course("JavaScript", "Программирование на JavaScript", 2),
+                       new Course("Go", "Программирование на Go", 3));
                var header = "Курсы по программированию";
                var page = new CoursesPage(courses, header);
                ctx.render("courses/index.jte", model("page", page));
           });
 
+          app.get("/", ctx -> ctx.render("index.jte"));
+
           app.start(7070);
+     }
+
+     public static Course findCourse(List<Course> listCourses, long idCourse) {
+          Course courseWithId = listCourses.stream()
+                  .filter(u -> idCourse == u.getId())
+                  .findFirst()
+                  .orElse(null);
+          return courseWithId;
      }
 }
 
