53ef8c2c98f9e436166dbb605c0c89ba1451338d
Evgeny_Ch <gek_sh@mail.ru>
2024-05-06 00:09:46 +0300

    Added term

--- src/main/java/org/example/hexlet/dto/courses/CoursesPage.java
+++ src/main/java/org/example/hexlet/dto/courses/CoursesPage.java
@@ -1,16 +1,22 @@
 package org.example.hexlet.dto.courses;
 
+import lombok.AllArgsConstructor;
 import org.example.hexlet.model.Course;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
+@AllArgsConstructor
 public class CoursesPage {
     private List<Course> courses;
     private String header;
-    public CoursesPage(List<Course> courses, String header) {
-        this.courses = courses;
-        this.header = header;
-    }
+    private String term;
+
+    //public CoursesPage(List<Course> courses) {
+    //}
+    //public CoursesPage(List<Course> courses, String header) {
+    //    this.courses = courses;
+    //    this.header = header;
+    //}
 }
