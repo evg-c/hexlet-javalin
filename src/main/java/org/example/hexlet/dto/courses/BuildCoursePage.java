19ccad1d0edcd5b05583cd422f98b4ba74997ee6
Evgeny_Ch <gek_sh@mail.ru>
2024-06-06 00:10:33 +0300

    Added validation courses

---
+++ src/main/java/org/example/hexlet/dto/courses/BuildCoursePage.java
@@ -0,0 +1,18 @@
+package org.example.hexlet.dto.courses;
+
+import io.javalin.validation.ValidationError;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class BuildCoursePage {
+    private String name;
+    private String description;
+    private Map<String, List<ValidationError<Object>>> errors;
+}
